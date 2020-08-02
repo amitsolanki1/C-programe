3f3445d74837fd7409bcd81e3459301408a32c04
amitsolanki1 <45381333+amitsolanki1@users.noreply.github.com>
2020-08-01 21:28:59 -0700

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{	
+	int a,b,sum=0;
+	
+		printf("Enter two no. to calculate sum : \n");
+		scanf("%d",&a);
+		scanf("%d",&b);	
+	//sum formula
+		sum=a+b;
+		printf("\nSum of two no. is : %d ",sum);
+		return 0;
+	
+}
